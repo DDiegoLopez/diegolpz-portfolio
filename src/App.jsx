59c0b84f40a9176d7053be9cfa8737c45c4541f5
Diego <89713249+DDiegoLopez@users.noreply.github.com>
2025-02-06 16:31:32 -0600

    add speed insights from vercel

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import About from './sections/About'
 import Projects from './sections/Projects'
 import Contact from './sections/Contact'
 import Footer from './sections/Footer'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
+      <SpeedInsights />
       <Navbar />
       <Hero />
       <About />
