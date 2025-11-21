5919e767222ca0ffa39dad532b12219066c6fdea
Diego <dlc2002@proton.me>
2025-11-20 19:03:41 -0600

    fix: delete problematic model and fix rings model position

--- src/constants/index.js
+++ src/constants/index.js
@@ -268,19 +268,12 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
       ? [5, 4, 0]
       : [12, 3, 0],
     ringPosition: isSmall
-      ? [-5, 7, 0]
+      ? [-3, 6, 0]
       : isMobile
-      ? [-10, 10, 0]
+      ? [-4, 8, 0]
       : isTablet
-      ? [-12, 10, 0]
-      : [-24, 10, 0],
-    targetPosition: isSmall
-      ? [-5, -10, -10]
-      : isMobile
-      ? [-9, -10, -10]
-      : isTablet
-      ? [-11, -7, -10]
-      : [-13, -13, -10],
+      ? [-9, 6, 0]
+      : [-10, 1, 0],
   };
 };
 
